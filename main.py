dfa543e9f6157cc44c1a33df2a8ab58a31f9d318
P Manuel Fernández <136108943+pmafergon@users.noreply.github.com>
2024-03-20 19:21:14 +0100

    'añadidos soluciones de devoluciones'

---
+++ main.py
@@ -0,0 +1,55 @@
+# ****entrada del ticket de compra****
+
+def entradaticket(totalproductos):
+    producto = input("Ingrese el producto: ")
+    uds = int(input("Ingrese el número de unidades: "))
+    if uds < 0:
+        for i in totalproductos:
+            if i == producto:
+                print(f"Se va a proceder a la devolución de {uds*(-1)} uds de {producto}")
+                precio = float(input("Ingrese el precio unitario del poducto: "))
+                a = [f"{uds} uds - {producto} - {precio} €"]
+                ticket=[a]
+                precio = precio * uds
+                total=[precio]
+                return ticket, total, producto
+        else:
+            print("Este producto no se puede devolver porque no se encuentra en el ticket del cliente, vuelva a ingresar el producto comprado")
+            return [],[],[]
+    else:
+        precio = float(input("Ingrese el precio unitario del producto: "))
+        a = [f"{uds} uds - {producto} - {precio} €"]
+        ticket=[a]
+        precio = precio * uds
+        total=[precio]
+        
+        return ticket, total, producto
+
+ticketcliente = []
+totalcliente = []
+totalproductos=[]
+ti, to, pro = entradaticket(totalproductos)
+ticketcliente.append(ti)
+totalcliente += to
+totalproductos.append(pro)
+
+control = True
+
+while control:
+    seguir = input("¿Desea añadir otro producto? S/N: ")
+    if seguir.lower() == "s":
+        ti, to, pro = entradaticket(totalproductos)
+        ticketcliente.append(ti)
+        totalcliente += to
+        totalproductos.append(pro)
+    elif seguir.lower() == "n":
+        control = False
+    else:
+        print(
+            'Algo falló, por favor, ingrese "S" si desea añadir otro producto, "N" para finalizar el programa'
+            )
+
+for i in range(len(ticketcliente)):
+    if ticketcliente[i]  != []:
+        print(ticketcliente[i], end="\n")
+print(f"El precio total del ticket es {sum(totalcliente)}€")
---
+++ pruebas de control.py
@@ -0,0 +1,3 @@
+for i in range (6):
+    a=[i]
+    print(a)
